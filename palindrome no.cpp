0bf988072ff40b67428f8006609d583d52539e46
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2023-02-02 22:29:31 +0530

    Create palindrome no.cpp

---
+++ palindrome no.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0)
+        {
+            return false;
+        }
+        long int a=x, b=0;
+        while (x) 
+        {
+            b=b*10+ x%10;
+            x/=10;
+        }
+        return a==b;
+        }
+};
